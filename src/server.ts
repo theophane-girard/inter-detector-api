664e107a93d91df99b0e4a8e781eccbfe5f804b5
Théophane Girard <tgirard@nsi.admr.org>
2021-06-27 19:52:36 +0200

    https test

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,26 @@
 import app from "./app";
+import https from "https"
+import fs from "fs"
 
 const PORT = process.env.PORT || 3000;
+let keyPath = "/etc/letsencrypt/live/theophane-girard.site/privkey.pem"
+let certPath = "/etc/letsencrypt/live/theophane-girard.site/fullchain.pem"
+let caPath = "/etc/letsencrypt/live/theophane-girard.site/chain.pem"
 
-app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
+let key = fs.existsSync(keyPath)
+let cert = fs.existsSync(certPath)
+let ca = fs.existsSync(caPath)
+
+if (key && cert && ca) {
+  https.createServer(
+    {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+      ca: fs.readFileSync(caPath),
+    },
+    app
+  )
+  console.log(`app listening on port ${PORT} with https!`)
+} else {
+  app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
+}
